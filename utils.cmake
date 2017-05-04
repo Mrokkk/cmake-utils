0605ae239e7d087b7ec2b9c84a6b12695461a513
Mrokkk <mrokk@protonmail.com>
2017-05-04 11:20:17 +0200

    Pass additional function params to executable

--- utils.cmake
+++ utils.cmake
@@ -2,7 +2,7 @@ function(add_run_target executable target-name)
     add_custom_target(
         ${target-name}
         DEPENDS ${executable}
-        COMMAND ./${executable}
+        COMMAND ./${executable} ${ARGV2}
         COMMENT "Running ${executable}"
     )
 endfunction(add_run_target)
@@ -11,7 +11,7 @@ function(add_valgrind_target executable target-name)
     add_custom_target(
         ${target-name}
         DEPENDS ${executable}
-        COMMAND valgrind --error-exitcode=1 --leak-check=full ./${executable}
+        COMMAND valgrind --error-exitcode=1 --leak-check=full ./${executable} ${ARGV2}
         COMMENT "Running ${executable} with Valgrind"
     )
 endfunction(add_valgrind_target)
